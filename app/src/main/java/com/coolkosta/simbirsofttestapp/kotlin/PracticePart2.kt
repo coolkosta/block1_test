7042c7eabce429ce440419357d32909d661ec83c
coolkosta <kostia.borzakov@gmail.com>
2024-03-08 18:15:55 +0300

    Создание класса User

--- app/src/main/java/com/coolkosta/simbirsofttestapp/kotlin/PracticePart2.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/kotlin/PracticePart2.kt
@@ -1,9 +1,22 @@
 package com.coolkosta.simbirsofttestapp.kotlin
 
+import java.util.Date
+
 /**
  * Создать enum Type с константами DEMO и FULL.
  */
 enum class Type {
     DEMO,
     FULL
+}
+
+/**
+ * Реализовать класс данных User с полями id, name, age и type.
+ * У класса User создать ленивое свойство startTime, в котором получаем текущее время.
+ */
+
+data class User(val id: Int, val name: String, val age: Int, val type: Type) {
+    val startTime: Date by lazy {
+        Date()
+    }
 }
