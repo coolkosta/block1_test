a150d9ccf7d0b8355d3015b5e78c7560903e84ee
coolkosta <kostia.borzakov@gmail.com>
2024-03-08 18:32:32 +0300

    Реализация изолированного класса Action и его наследников

--- app/src/main/java/com/coolkosta/simbirsofttestapp/kotlin/PracticePart2.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/kotlin/PracticePart2.kt
@@ -126,3 +126,12 @@ inline fun auth(user: User, updateCache: () -> Unit, authCallback: AuthCallback)
         authCallback.authFailed()
     }
 }
+
+/**
+ * Реализовать изолированный класс Action и его наследников – Registration,Login и Logout.
+ * Login должен принимать в качестве параметра экземпляр класса User.
+ */
+sealed class Action
+class Registration : Action()
+class Login(val user: User) : Action()
+class Logout : Action()
