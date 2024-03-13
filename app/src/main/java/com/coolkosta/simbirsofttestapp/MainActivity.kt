53fb6f941ae8c183454d9361400837afd710a050
coolkosta <kostia.borzakov@gmail.com>
2024-03-13 14:36:19 +0300

    Исправление размещения элементов Splash экрана

--- app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
@@ -2,19 +2,10 @@ package com.coolkosta.simbirsofttestapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.coolkosta.simbirsofttestapp.ui.theme.SimbirSoftTestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
