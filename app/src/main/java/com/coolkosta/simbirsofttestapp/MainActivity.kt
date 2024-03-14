9fd6cdd9589cbd9fa44ad62afc152bc3e810e8ba
coolkosta <kostia.borzakov@gmail.com>
2024-03-14 17:26:03 +0300

    Верстка экрана Profile

--- app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
+        setContentView(R.layout.activity_main)
     }
 }
