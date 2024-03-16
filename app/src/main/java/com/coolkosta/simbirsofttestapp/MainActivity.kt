2d3e3659fa0cbc2b56d7f71aecbbc7165ba10aa9
coolkosta <kostia.borzakov@gmail.com>
2024-03-16 12:05:24 +0300

    Установка для BottomNavigationView пункта "Профиль" по умолчанию

--- app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.coolkosta.simbirsofttestapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.selectedItemId = R.id.profile
     }
 }
