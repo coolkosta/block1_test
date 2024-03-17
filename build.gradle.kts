9c9aa56207573f14ff9068aa5d003ad61b275f96
coolkosta <kostia.borzakov@gmail.com>
2024-03-17 12:41:21 +0300

    Добавление плагина ktlint, реализация Collapsing toolbar-а

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to al
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
