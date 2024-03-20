5a1b107e4bb211edddfc2ebb6bb6efbe78007715
oleg lazarev <oleg.lazarev@simbirsoft.com>
2024-03-20 22:55:18 +0300

    fix splash screen

--- app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
+++ app/src/main/java/com/coolkosta/simbirsofttestapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.coolkosta.simbirsofttestapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
