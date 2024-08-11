2da10e8577ea15121275e4f1c25860e2f3ae4e50
oleg lazarev <oleg.lazarev@simbirsoft.com>
2024-08-11 22:12:29 +0400

    fix

--- features/news/src/main/java/com/coolkosta/news/presentation/screen/eventDetailFragment/NotificationReceiver.kt
+++ features/news/src/main/java/com/coolkosta/news/presentation/screen/eventDetailFragment/NotificationReceiver.kt
@@ -1,66 +1,34 @@
 package com.coolkosta.news.presentation.screen.eventDetailFragment
 
-import android.Manifest
-import android.app.Notification.EXTRA_NOTIFICATION_ID
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.ALARM_SERVICE
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.IntentCompat
-import com.coolkosta.core.util.Constants.NAME_EVENT_EXTRA
-import com.coolkosta.news.R
-import com.coolkosta.news.di.NewsComponentProvider
-import com.coolkosta.news.domain.model.EventEntity
-import com.coolkosta.news.presentation.screen.eventDetailFragment.NotificationWorker.Companion.ID_DONATION_NOTIFICATION
 
-
+// класс обрабатывает логику нажатия кнопки "напомнить позже", также обрабатывает отложенный PendingIntent на показ уведомления через задержку
+// логика такая - при нажатии кнопки отложить срабатывает BroadcastReceiver, приходит интент с action = ACTION_REMINDER_LATER
+// мы через alarm manager планируем задачу, но результат этой задачи - тоже PendingIntent, мы не можем из alarm manager через задержку напрямую отобразить уведомление
+// для удобства используем этот же BroadcastReceiver. в PendingIntent ложим тот же интенет (который мы сформировали в NotificationUtils, и который хранит инфу о событи),
+// но меняем у него action. Когда через задержку приходит intent.action == ACTION_SHOW - вызываем отображение уведомления, но в измененном виде (без кнопки напомнить).
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == NotificationWorker.ACTION_REMINDER_LATER) {
-            IntentCompat.getParcelableExtra(
-                intent,
-                EXTRA_NOTIFICATION_ID,
-                EventEntity::class.java
-            )?.let {
-                val builder = NotificationCompat.Builder(
-                    context,
-                    NotificationWorker.ID_CHANNEL_DONATION_NOTIFICATION
-                )
-                    .setSmallIcon(com.coolkosta.core.R.drawable.ic_coins)
-                    .setContentTitle(it.title)
-                    .setContentText(context.getString(R.string.snooze_notification_description_text))
-                    .setStyle(
-                        NotificationCompat.BigTextStyle()
-                            .bigText(context.getString(R.string.snooze_notification_description_text))
-                    )
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setContentIntent(openDetailScreenPendingIntent(context, it))
-                    .setAutoCancel(true)
-                val notificationManager = NotificationManagerCompat.from(context)
-                if (ActivityCompat.checkSelfPermission(
-                        context, Manifest.permission.POST_NOTIFICATIONS
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    notificationManager.notify(ID_DONATION_NOTIFICATION, builder.build())
-                }
-            }
+        if (intent.action == ACTION_REMINDER_LATER) {
+            intent.action = ACTION_SHOW
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
+            val triggerTime = System.currentTimeMillis() + 1 * 60 * 1000
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent)
+            //при нажатии на кнопку "напомнить" нужно скрыть текущее
+            NotificationUtils.cancelCurrent(context)
+        } else if (intent.action == ACTION_SHOW) {
+            NotificationUtils.showRemindedNotification(intent, context)
         }
     }
 
-    private fun openDetailScreenPendingIntent(context: Context, event: EventEntity): PendingIntent {
-        val intent = (context.applicationContext as NewsComponentProvider).getNewsComponent()
-            .intentActivity()
-            .getActivityIntent()
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        intent.putExtra(NAME_EVENT_EXTRA, event)
-
-        return PendingIntent.getActivity(
-            context, 0, intent, PendingIntent.FLAG_IMMUTABLE
-        )
+    companion object {
+        const val ACTION_REMINDER_LATER = "remind_later"
+        const val ACTION_SHOW = "action_show"
     }
-
 }
--- features/news/src/main/java/com/coolkosta/news/presentation/screen/eventDetailFragment/NotificationWorker.kt
+++ features/news/src/main/java/com/coolkosta/news/presentation/screen/eventDetailFragment/NotificationWorker.kt
@@ -1,116 +1,24 @@
 package com.coolkosta.news.presentation.screen.eventDetailFragment
 
-import android.Manifest
-import android.app.AlarmManager
-import android.app.Notification.EXTRA_NOTIFICATION_ID
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.coolkosta.core.util.Constants.NAME_EVENT_EXTRA
-import com.coolkosta.news.R
-import com.coolkosta.news.R.string.notification_description_tex
-import com.coolkosta.news.di.NewsComponentProvider
 import com.coolkosta.news.domain.model.EventEntity
 import com.coolkosta.news.presentation.screen.eventDetailFragment.EventDetailViewModel.Companion.KEY_AMOUNT_DATA
 import com.coolkosta.news.presentation.screen.eventDetailFragment.EventDetailViewModel.Companion.KEY_EVENT_DATA
 import com.google.gson.Gson
 
-class NotificationWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+//класс отвечает только за обработку запланированной задачи. Не нарушаем принципы Single responsibility.
+// Формирование и показ нотификации отдельно
+class NotificationWorker(private val context: Context, params: WorkerParameters) : Worker(context, params) {
 
     override fun doWork(): Result {
 
         val eventDataString = inputData.getString(KEY_EVENT_DATA)
-        val event = Gson().fromJson(eventDataString, EventEntity::class.java)
+        val event: EventEntity = Gson().fromJson(eventDataString, EventEntity::class.java)
         val amount = inputData.getInt(KEY_AMOUNT_DATA, 0)
 
-        createNotificationChannel()
-        val builder =
-            NotificationCompat.Builder(applicationContext, ID_CHANNEL_DONATION_NOTIFICATION)
-                .setSmallIcon(com.coolkosta.core.R.drawable.ic_coins).setContentTitle(event.title)
-                .setContentText(applicationContext.getString(notification_description_tex, amount))
-                .setStyle(
-                    NotificationCompat.BigTextStyle()
-                        .bigText(applicationContext.getString(notification_description_tex, amount))
-                )
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(openDetailScreenPendingIntent(event))
-                .addAction(
-                    com.coolkosta.core.R.drawable.ic_history,
-                    applicationContext.getString(R.string.remind_later_text),
-                    setReminderLater(event)
-                )
-                .setAutoCancel(true)
-
-        val notificationManager = NotificationManagerCompat.from(applicationContext)
-        if (ActivityCompat.checkSelfPermission(
-                applicationContext, Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            notificationManager.notify(ID_DONATION_NOTIFICATION, builder.build())
-        }
-
+        NotificationUtils.showNotification(context, amount, event)
         return Result.success()
-    }
-
-    private fun createNotificationChannel() {
-        val name = NAME_CHANNEL_DONATION_NOTIFICATION
-        val descriptionText = DESCRIPTION_DONATION_NOTIFICATION
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel =
-            NotificationChannel(ID_CHANNEL_DONATION_NOTIFICATION, name, importance).apply {
-                description = descriptionText
-            }
-
-        val notificationManager: NotificationManager =
-            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-
-    }
-
-    private fun openDetailScreenPendingIntent(event: EventEntity): PendingIntent {
-        val intent =
-            (applicationContext as NewsComponentProvider).getNewsComponent().intentActivity()
-                .getActivityIntent()
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        intent.putExtra(NAME_EVENT_EXTRA, event)
-
-        return PendingIntent.getActivity(
-            applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE
-        )
-    }
-
-    private fun setReminderLater(event: EventEntity): PendingIntent {
-        val reminderIntent = Intent(applicationContext, NotificationReceiver::class.java).apply {
-            action = ACTION_REMINDER_LATER
-            putExtra(EXTRA_NOTIFICATION_ID, event)
-        }
-
-        val pendingIntent = PendingIntent.getBroadcast(
-            applicationContext,
-            0,
-            reminderIntent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-        val alarmManager =
-            applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val triggerTime = System.currentTimeMillis() + 1 * 60 * 1000
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent)
-        return pendingIntent
-    }
-
-    companion object {
-        const val ID_CHANNEL_DONATION_NOTIFICATION = "ID_CHANNEL_DONATION_NOTIFICATION"
-        const val ID_DONATION_NOTIFICATION = 1
-        private const val NAME_CHANNEL_DONATION_NOTIFICATION = "donation notification"
-        private const val DESCRIPTION_DONATION_NOTIFICATION = "notifications sent donations"
-        const val ACTION_REMINDER_LATER = "remind_later"
     }
 }
